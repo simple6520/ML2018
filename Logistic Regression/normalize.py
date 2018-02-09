ac7da42aa4e4d859b2766bcf0da9e78b46203575
simple6520 <r04942131@ntu.edu.tw>
2018-02-09 15:58:22 +0800

    Create normalize.py

---
+++ Logistic Regression/normalize.py
@@ -0,0 +1,13 @@
+def normalize(X_all, X_test):
+    # Feature normalization with train and test X
+    X_train_test = np.concatenate((X_all, X_test))
+    mu = (sum(X_train_test) / X_train_test.shape[0])
+    sigma = np.std(X_train_test, axis=0)
+    mu = np.tile(mu, (X_train_test.shape[0], 1))
+    sigma = np.tile(sigma, (X_train_test.shape[0], 1))
+    X_train_test_normed = (X_train_test - mu) / sigma
+
+    # Split to train, test again
+    X_all = X_train_test_normed[0:X_all.shape[0]]
+    X_test = X_train_test_normed[X_all.shape[0]:]
+    return X_all, X_test
